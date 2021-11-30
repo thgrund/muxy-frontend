95e94c1ce690b22c46bc0329196659df0b0efb37
Bernard Gray <bernard.gray@gmail.com>
2021-11-30 11:58:40 +0000

    update date desc

--- src/app/components/EventHeader.tsx
+++ src/app/components/EventHeader.tsx
@@ -6,7 +6,7 @@ function EventHeader(): ReactElement {
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1>Longest Night Marathon</h1>
-            <h2>December 21th - December 22th</h2>
+            <h2>December 21st - December 22nd</h2>
             <p className="link-paragraph"><a href={"https://www.youtube.com/eulerroom/live"}>https://www.youtube.com/eulerroom/live</a></p>
             <p className="link-paragraph"><a href={"https://www.twitch.tv/eulerroom"}>https://www.twitch.tv/eulerroom</a></p>
         </header>
