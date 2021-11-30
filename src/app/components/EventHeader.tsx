20ceb29a0c9e3236f2f4f7c45548245e2a103063
Bernard Gray <bernard.gray@gmail.com>
2021-11-30 19:31:46 +0000

    add some hr styling

--- src/app/components/EventHeader.tsx
+++ src/app/components/EventHeader.tsx
@@ -7,7 +7,9 @@ function EventHeader(): ReactElement {
             <img src={logo} className="App-logo" alt="logo" />
             <h1>Longest Night Marathon</h1>
             <h2>December 21st - December 22nd</h2>
+            <hr/>
             <h3>Signups Opening Soon! Watch this space...</h3>
+            <hr/>
             <p className="link-paragraph"><a href={"https://www.youtube.com/eulerroom/live"}>https://www.youtube.com/eulerroom/live</a></p>
             <p className="link-paragraph"><a href={"https://www.twitch.tv/eulerroom"}>https://www.twitch.tv/eulerroom</a></p>
         </header>
