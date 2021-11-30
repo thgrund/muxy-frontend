f1038be3b4453edd4d4a42e98619c474e357aa80
Bernard Gray <bernard.gray@gmail.com>
2021-11-30 19:11:21 +0000

    add some sign up info

--- src/app/components/EventHeader.tsx
+++ src/app/components/EventHeader.tsx
@@ -7,6 +7,7 @@ function EventHeader(): ReactElement {
             <img src={logo} className="App-logo" alt="logo" />
             <h1>Longest Night Marathon</h1>
             <h2>December 21st - December 22nd</h2>
+            <h3>Signups Opening Soon! Watch this space...</h3>
             <p className="link-paragraph"><a href={"https://www.youtube.com/eulerroom/live"}>https://www.youtube.com/eulerroom/live</a></p>
             <p className="link-paragraph"><a href={"https://www.twitch.tv/eulerroom"}>https://www.twitch.tv/eulerroom</a></p>
         </header>
