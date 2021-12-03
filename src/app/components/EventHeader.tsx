259842ca6b28b11ebc42950369420900a27d6306
Bernard Gray <bernard.gray@gmail.com>
2021-12-03 08:03:54 +0000

    add year, presents

--- src/app/components/EventHeader.tsx
+++ src/app/components/EventHeader.tsx
@@ -5,10 +5,13 @@ function EventHeader(): ReactElement {
     return (
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1>Longest Night Marathon</h1>
+            <h4>Tidal Club Presents ...</h4>
+            <h1>The Longest Night</h1>
             <h2>December 21st - December 22nd</h2>
+            <h2>2021</h2>
             <hr/>
-            <h3>Signups Opening Soon! Watch this space...</h3>
+            <h3>Signups Opening Soon!</h3>
+            <h4>Watch this space...</h4>
             <hr/>
             <p className="link-paragraph"><a href={"https://www.youtube.com/eulerroom/live"}>https://www.youtube.com/eulerroom/live</a></p>
             <p className="link-paragraph"><a href={"https://www.twitch.tv/eulerroom"}>https://www.twitch.tv/eulerroom</a></p>
