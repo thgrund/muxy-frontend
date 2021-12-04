9e0ab4f3c2d20b07d229560c0e477636ad085249
Damián Silvani <munshkr@gmail.com>
2021-12-04 18:11:29 -0300

    Delete slots

--- src/app/components/PerformanceCard.tsx
+++ src/app/components/PerformanceCard.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, useState } from "react";
 import "../../assets/css/PerformanceCard.css";
 import { EmptyMuxyStream, MuxyStream } from "../types";
 import PerformanceCreateForm from "./PerformanceCreateForm";
+import PerformanceDestroyForm from "./PerformanceDestroyForm";
 import { DateTime } from "luxon";
 
 interface Props {
@@ -10,8 +11,14 @@ interface Props {
   eventUrl: string;
 }
 
-const PerformanceCard = ({ muxyStream, cycleNo, eventUrl }: Props): ReactElement => {
+const PerformanceCard = ({
+  muxyStream,
+  cycleNo,
+  eventUrl,
+}: Props): ReactElement => {
   const [inCreateMode, setInCreateMode] = useState<boolean>(false);
+  const [inRemoveMode, setInRemoveMode] = useState<boolean>(false);
+  const [removed, setRemoved] = useState<boolean>(false);
 
   const startsAtHs = DateTime.fromISO(muxyStream.starts_at).toFormat("HH:mm");
   const endsAtHs = DateTime.fromISO(muxyStream.ends_at).toFormat("HH:mm");
@@ -21,6 +28,9 @@ const PerformanceCard = ({ muxyStream, cycleNo, eventUrl }: Props): ReactElement
     const { publisher_name, location, description, timezone } = muxyStream;
     text = [publisher_name, location, description, timezone].join(" / ");
   }
+
+  const handleRemoveClick = () => setInRemoveMode((prevState) => !prevState);
+  const handleRemove = () => setRemoved(true);
 
   return (
     <div className="card">
@@ -37,12 +47,29 @@ const PerformanceCard = ({ muxyStream, cycleNo, eventUrl }: Props): ReactElement
             </button>
           )}
         </p>
+        {removed && <p>You have removed your slot succesfully.</p>}
         {inCreateMode ? (
-          <PerformanceCreateForm eventUrl={eventUrl} startsAt={muxyStream.starts_at} endsAt={muxyStream.ends_at} />
+          <PerformanceCreateForm
+            eventUrl={eventUrl}
+            startsAt={muxyStream.starts_at}
+            endsAt={muxyStream.ends_at}
+          />
         ) : (
           <>
-            <p className="card-text">{text || ""}</p>
-            {text && <button className="card-button">Remove</button>}
+            <p className="card-text">{removed ? "[REMOVED]" : text || ""}</p>
+            {!removed && text && (
+              <>
+                <button onClick={handleRemoveClick} className="card-button">
+                  Remove
+                </button>
+                {inRemoveMode && "url" in muxyStream && (
+                  <PerformanceDestroyForm
+                    streamUrl={muxyStream.url}
+                    onRemove={handleRemove}
+                  />
+                )}
+              </>
+            )}
           </>
         )}
 
---
+++ src/app/components/PerformanceDestroyForm.tsx
@@ -0,0 +1,72 @@
+import React, { ReactElement, useState } from "react";
+
+interface Props {
+  streamUrl: string;
+  onRemove: () => void;
+}
+
+function PerformanceDestroyForm({ streamUrl, onRemove }: Props): ReactElement {
+  const [streamKey, setStreamKey] = useState<string>("");
+  const [failed, setFailed] = useState<boolean>(false);
+  const muxyApiKey: string = process.env.REACT_APP_MUXY_API_KEY as string;
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setFailed(false);
+
+    const headers = new Headers({
+      Authorization: `Api-Key ${muxyApiKey}`,
+      "X-Stream-Key": streamKey,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    });
+
+    fetch(streamUrl, {
+      method: "DELETE",
+      headers,
+    })
+      .then((res) => {
+        if (res.ok) {
+          setStreamKey("");
+          onRemove && onRemove();
+        } else {
+          setFailed(true);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        setFailed(true);
+      });
+  };
+
+  return (
+    <>
+      {
+        <form className="PerformanceCreateForm" onSubmit={handleSubmit}>
+          <p>
+            Enter your streaming key to confirm you want to remove your slot. If
+            you do not remember your streaming key, please contact the event
+            organizer.
+          </p>
+
+          {failed && (
+            <p style={{ color: "red" }}>
+              Something went wrong, please try again.
+            </p>
+          )}
+          <input
+            id="key"
+            type="text"
+            placeholder="Stream key"
+            value={streamKey}
+            onChange={(e) => setStreamKey(e.target.value)}
+            required
+          />
+          <input type="submit" className="card-button" value="Remove slot" />
+        </form>
+      }
+    </>
+  );
+}
+
+export default PerformanceDestroyForm;
