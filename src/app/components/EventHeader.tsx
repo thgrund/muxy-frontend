a8440106294f89b39363e1bf62f86ed0ffb600fd
Thomas Grund <thomas.grund@stud.htwk-leipzig.de>
2021-12-05 19:50:57 +0100

    Change the event header to display the event date dynamically.

--- src/app/components/EventHeader.tsx
+++ src/app/components/EventHeader.tsx
@@ -13,12 +13,9 @@ function EventHeader({event}:Props): ReactElement {
             <img src={logo} className="App-logo" alt="logo" />
             <h4>Tidal Club Presents ...</h4>
             <h1>The Longest Night</h1>
-            <h2>December 21st {event && DateTime.fromISO(event.starts_at).toFormat("HH:mm")} - December 22nd {event && DateTime.fromISO(event.ends_at).toFormat("HH:mm")} 2021</h2>
+            <h2> {event && DateTime.fromISO(event.starts_at).toFormat("dd. LLLL HH:mm")} - {event && DateTime.fromISO(event.ends_at).toFormat("dd. LLLL HH:mm")} {event && DateTime.fromISO(event.starts_at).toFormat("yyyy")}</h2>
             <hr/>
             <h2>Your timezone: {Intl.DateTimeFormat().resolvedOptions().timeZone}</h2>
-            <hr/>
-            <h3>Signups Opening Soon!</h3>
-            <h4>Watch this space...</h4>
             <hr/>
             <p className="link-paragraph"><a href={"https://www.youtube.com/eulerroom/live"}>https://www.youtube.com/eulerroom/live</a></p>
             <p className="link-paragraph"><a href={"https://www.twitch.tv/eulerroom"}>https://www.twitch.tv/eulerroom</a></p>
